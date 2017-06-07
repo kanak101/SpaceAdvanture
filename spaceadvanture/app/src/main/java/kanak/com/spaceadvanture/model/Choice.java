2bca990cf9ac82aeacb36af72b69430d824c52e5
kanak101 <thaider1@gmail.com>
2017-06-06 21:06:59 -0400

    Project Files

---
+++ spaceadvanture/app/src/main/java/kanak/com/spaceadvanture/model/Choice.java
@@ -0,0 +1,31 @@
+package kanak.com.spaceadvanture.model;
+
+/**
+ * Created by kanak on 6/1/2016.
+ */
+public class Choice {
+    private String mText;
+    private int mNextPage;
+
+    public Choice(String text, int nextPage)
+    {
+        mText = text;
+        mNextPage = nextPage;
+
+    }
+    public String getText() {
+        return mText;
+    }
+
+    public void setText(String text) {
+        mText = text;
+    }
+
+    public int getNextPage() {
+        return mNextPage;
+    }
+
+    public void setNextPage(int nextPage) {
+        mNextPage = nextPage;
+    }
+}
